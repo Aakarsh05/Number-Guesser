678fa0beadcc4a1291cc7c72208e41faa9dae7de
Pritam Panda <89348093+Pritam0077@users.noreply.github.com>
2022-04-08 12:19:02 +0530

    Rectified the error

--- main.js
+++ main.js
@@ -1,19 +1,19 @@
 let btn=document.getElementById('btn');
 let output=document.getElementById('outputtext');
 
-let number=[Math.floor(Math.random() * 100)]
+let number=Math.floor(Math.random() * 100)+1;
 
 btn .addEventListener('click',function(){
     let input=document.getElementById('userinput').value;
     if(input == number)
     {
-        output.innerHTML =" You guessed right ,your number was  ' ${number};'"
+        output.innerHTML =" You guessed right ,the number was "+number;
     }
     else if(input < number)
     {
         output.innerHTML=" You guessed too low!"
     };
-    if(input >number)
+    if(input > number)
     {
         output.innerHTML ="You gussed too high!"
     }
